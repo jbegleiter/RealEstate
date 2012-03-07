c1aa32a0d15a6c31ea82b1845b1666c373472dc7
Jason Begs <jbegleiter@pinpoynthealth.com>
2012-03-07 03:42:00 +0000

    Parses fields

--- python/lib.py
+++ python/lib.py
@@ -4,8 +4,20 @@ def callAPI(func, zip):
 	url = 'http://api.trulia.com/webservices.php?library=LocationInfo&function='+func+'&state='+zip+'&apikey=gysqg4zr2z65ycpzxzm47jam'
 	ufile = urllib.urlopen(url)
 	utext = ufile.read()
-	print utext
-	
+	sqlFields(utext)
+	##print utext
+
+##temporary	--> Find all API fields returned
+def sqlFields(utext):
+	fields = []
+	t = '<name>Woodlake</name><longitude>-119.105850128324</longitude>'
+	tuples = re.findall(r'<([\w\d\+\.\s/-]+)>([\w\d\+\.\s/-]+)</([\w\d\+\.\s/-]+)>',utext)
+	for tuple in tuples:
+		if not (tuple[0] in fields):
+			fields.append(tuple[0])
+			print tuple[0]
+			
+
 
 def main():
 	callAPI('getCitiesInState','CA')
