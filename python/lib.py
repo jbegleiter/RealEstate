0014b82fe043598f8971410c20a43a77e5f5f83d
Jason Begs <jbegleiter@pinpoynthealth.com>
2012-03-13 04:11:58 +0000

    psuedo biatch

--- python/lib.py
+++ python/lib.py
@@ -11,7 +11,6 @@ class dataPull():
 		request = cursor.execute(query)
 		for r in range(request):
 			print cursor.fetchone()
-
 		cursor.close()
 
 
@@ -53,6 +52,56 @@ class dbQuery:
 		request = cursor.execute(query)
 		if (range(request) > 0):
 			for r in range(request):
-				print cursor.fetchrow() ##have this added to a dictionary
+				print cursor.fetchone() ##have this added to a dictionary
 		else:
 			return false
+
+
+class APIcall:
+	func = ''
+	apikey = 'gysqg4zr2z65ycpzxzm47jam' ##Make this a db table (eventually)
+	root = 'http://api.trulia.com/webservices.php?library='
+	request_url = ''
+
+	def __init__(self):
+		print 'ahoy'
+
+	#def compose_request(self,func,population):
+		##lookup structure of request in db
+		#returns url
+
+	#def make_apicall(self,url):
+		#returns utext
+
+	#def parse_results(self,utext,func):
+		##lookup result fields in db
+		## insert into dictionary with field names as lookup key
+		## parse structure hardcoded for now, variables returned from db (above)
+		#returns result_data
+
+	#def save_results(self,result_data,func):
+		##look up appropriate sql table to save from a sql table
+		##Make sql call to insert into table
+		#return true/false/error message
+
+	#def display_results(self,results, func):
+		##this can be used if I'm not sure if I'm sure if the data should be saved
+		#print results
+
+class pullData:
+	request_input = {}
+	vehicle = ''
+	request_param = {}
+
+	def __init__(self):
+		print 'matey'
+
+	#def retrieve_data(self,specs):
+		##determine whether info exists in tables, if not make the necessary api call
+		##can use APIcall.displayresults if first want to check quality
+		##have to make db table here?
+		#call APIcall or pullData.dbpull
+
+	#def db_pull(self, specs)
+		#execute dbQuery(specs)
+
